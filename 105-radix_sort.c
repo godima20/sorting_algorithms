1ae5395422e6efcfb3433a7ee192faa3916d7cea
godima20 <godima20@gmail.com>
2022-10-12 19:44:17 +0000

    105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -8,7 +8,7 @@ void radix_sort(int *array, size_t size);
  * get_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array
- * 
+ *
  * Return: The maximum integer in the array.
  */
 int get_max(int *array, int size)
